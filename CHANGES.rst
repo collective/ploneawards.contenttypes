139bc0ea044c15ec49e76affd6b234e5f328dfe1
hvelarde <hector.velarde@gmail.com>
2013-09-18 16:25:11 -0300

    Refactor vote counting mechanism; votes are now counted directly from Twitter and Facebook using their API

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,10 +4,11 @@ Changelog
 0.2 (unreleased)
 ----------------
 
-- Nothing changed yet.
+- Refactor vote counting mechanism; votes are now counted directly from
+  Twitter and Facebook using their API. [hvelarde]
 
 
 0.1 (2012-08-29)
 ----------------
 
-- Initial release
+- Initial release.
--- ploneawards/contenttypes/backtweets.py
+++ ploneawards/contenttypes/backtweets.py
@@ -1,9 +1,11 @@
-import urllib
-from urllib2 import urlopen
-from bs4 import BeautifulSoup
-import time
+# -*- coding: utf-8 -*-
+
 from plone.memoize import ram
+from urllib2 import urlopen
+
+import json
 import logging
+import time
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -31,28 +33,48 @@ def _backtweets(url):
     by not catching many exceptions."""
     logger.debug("Refreshing vote count for: %s" % url)
     # any exceptions thrown here should be caught outside the cache
-    u = urlopen(backurl(url))
-    html = u.read()
-    u.close()
+    return _tweets(url) + _likes(url)
 
-    soup = BeautifulSoup(html)
-    # this will break if backtweet html changes...
+
+def _tweets(url):
+    """Use Twitter API to return the number of 'tweets' of url.
+
+    The API returns something like this:
+
+    {u'count': 511, u'url': u'http://plone.org/'}
+    """
+    API_URL = u'http://urls.api.twitter.com/1/urls/count.json?url='
+    info = json.load(urlopen(API_URL + url))
+    return int(info[u'count'])
+
+
+def _likes(url):
+    """Use Facebook API to return the number of 'likes' of url.
+
+    The API returns something like this:
+
+    {u'http://plone.org': {u'id': u'http://plone.org', u'shares': 380}}
+
+    Note that 'shares' is included only if the url has 'likes' indeed.
+    """
     try:
-        results = soup.find(id='results') \
-            .find('div', 'title').find('span', 'blue').string
-        return int(results)
-    except AttributeError:
-        # this is valid and indicates: no tweets
+        API_URL = u'https://graph.facebook.com/?ids='
+        info = json.load(urlopen(API_URL + url))
+        info = info[url]
+        return int(info[u'shares']) if u'shares' in info else 0
+    except TypeError:
+        # this is valid and indicates: no info
         # return a cacheable result count
         return 0
 
 
 def backurl(url):
-    return 'http://backtweets.com/search/?q=%s' % urllib.quote(url)
+    return 'http://tagboard.com/ploneawards'
 
 
 if __name__ == '__main__':
     # for fast prototyping
-    url = 'bit.ly/Sazv3E'
-    count = backtweets(url)
-    print(count)
+    url = 'http://plone.org'
+    print _tweets(url)
+    print _likes(url)
+    print backtweets(url)
